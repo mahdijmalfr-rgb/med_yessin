e33b84594f92fa7963d71f99f057fcc8d9f408b1
mahdijmalfr-rgb <mahdijmalfr@gmail.com>
2026-08-16 15:49:00 +0100

    Refactor SmartAnalyticBot with additional features

--- 22medyessine22.py
+++ 22medyessine22.py
@@ -159,7 +159,8 @@ if __name__ == "__main__":
     
     st.write(reponse1)
     # دالة زر Enter
-    raw_input = st.text_input("اكتب سؤالك أو الأمر هنا (مثال: search tunis أو translate hello):")
+    raw_input = st.text_input("اكتب سؤالك أو الأمر هنا (مثال: search او translate ثم اكمل الجملة)
+    :")
 
     # 2. التحقق والتنفيذ
     if raw_input:
