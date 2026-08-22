cda71309d601bed63ac6aaac93f86d352f3483d4
mahdijmalfr-rgb <mahdijmalfr@gmail.com>
2026-08-22 13:57:23 +0200

    Refactor SmartAnalyticBot and improve functionality

--- 22medyessine22.py
+++ 22medyessine22.py
@@ -91,9 +91,9 @@ class SmartAnalyticBot:
                      #return data.get("extract", "لا يوجد ملخص متاح.")
                  #else:
                      #return "لم يتم العثور على مقال بهذا الاسم."
-            return reponse        
-        except Exception as e:
-            return f"حدث خطأ في الاتصال: {e}"
+        return reponse        
+    except Exception as e:
+        return f"حدث خطأ في الاتصال: {e}"
         
     def process_message(self, raw_input):
         reponse = ""
