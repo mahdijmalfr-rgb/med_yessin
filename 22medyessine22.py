0626823a99cc72533e9a6ce6d142cde8f4092ccf
mahdijmalfr-rgb <mahdijmalfr@gmail.com>
2026-08-22 13:37:09 +0200

    Refactor SmartAnalyticBot and improve functionality

--- 22medyessine22.py
+++ 22medyessine22.py
@@ -67,9 +67,7 @@ class SmartAnalyticBot:
         url_ar = f"https://ar.wikipedia.org/api/rest_v1/page/summary/{query}"
         url_fr = f"https://fr.wikipedia.org/api/rest_v1/page/summary/{query}"
 
-        urll_ar = f"https://ar.wiktionary.org/w/api.php/{query}"
-        urll_fr = f"https://fr.wiktionary.org/w/api.php/{query}"
-        urll_en = f"https://en.wiktionary.org/w/api.php/{query}"
+        urll =f"https://ar.wiktionary.org/api/rest_v1/page/definition/{query}"  
         try:
             try:
                 user_lang = detect(query)
@@ -84,17 +82,7 @@ class SmartAnalyticBot:
                 response = requests.get(url_en, headers=headers, timeout=5)
                 
             if response.status_code == 200:
-                try:
-                    user_lang = detect(query)
-                except:
-                    user_lang = 'en'
-                
-                if user_lang == 'ar':
-                    response = requests.get(urll_ar, headers=headers, timeout=5)
-                elif user_lang == 'fr':
-                    response = requests.get(urll_fr, headers=headers, timeout=5)
-                else:
-                     response = requests.get(urll_en, headers=headers, timeout=5)
+                response = requests.get(urll, headers=headers, timeout=5)
                 if response.status_code == 200:
                     data = response.json()
                     return data.get("extract", "لا يوجد ملخص متاح.")
