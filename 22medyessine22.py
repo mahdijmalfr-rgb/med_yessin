bdfbed753f899942d9704aa5805d63a586b10452
mahdijmalfr-rgb <mahdijmalfr@gmail.com>
2026-09-05 13:13:40 +0100

    Refactor and optimize SmartAnalyticBot class

--- 22medyessine22.py
+++ 22medyessine22.py
@@ -160,9 +160,9 @@ class SmartAnalyticBot:
                             "مثال": example
                             })
                             import re
-                            first_entry = meanings[0]
+                            
                             clean_def = re.sub(r'<[^>]+>', '', first_def)
-                            part_of_speech = first_entry.get("partOfSpeech", "")
+                            part_of_speech = meanings.get("partOfSpeech", "")
 
                             return meanings
 
