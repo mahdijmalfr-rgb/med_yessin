86138db516ef84fa34bc6ea6fc1e75bca6fb2174
mahdijmalfr-rgb <mahdijmalfr@gmail.com>
2026-09-07 15:08:06 +0100

    Update 22medyessine22.py

--- 22medyessine22.py
+++ 22medyessine22.py
@@ -316,13 +316,13 @@ class SmartAnalyticBot:
                     for line in response.iter_lines():
                         if line:
                             
-                            gg =line.decode("utf-8")
-                            reponce+= gg
+                             
+                            reponce = f"{ line.decode("utf-8")}"
                             abc = 0
                 else:
                     
-                    gg=response.json()
-                    reponse+= gg
+                    
+                    reponse =f"{ response.json()}"
                     abc = 0
         return reponse 
 # ----------------- تشغيل واجهة Streamlit -----------------
