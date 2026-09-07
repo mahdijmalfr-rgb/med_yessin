ddae171448c4c287c073a24cd8c54287ce851a83
mahdijmalfr-rgb <mahdijmalfr@gmail.com>
2026-09-07 14:30:37 +0100

    Update 22medyessine22.py

--- 22medyessine22.py
+++ 22medyessine22.py
@@ -272,41 +272,41 @@ class SmartAnalyticBot:
                 reponse += f"🔧 **التصحيح المقترح:** {corrected_text}\n\n"
                 
 
-            invoke_url = "https://integrate.api.nvidia.com/v1/chat/completions"
-            stream = False
+                invoke_url = "https://integrate.api.nvidia.com/v1/chat/completions"
+                stream = False
 
-            headers = {
-                "Authorization": "Bearer $NVIDIA_API_KEY",
-                "Accept": "text/event-stream" if stream else "application/json",
-            }
+                headers = {
+                    "Authorization": "Bearer $NVIDIA_API_KEY",
+                    "Accept": "text/event-stream" if stream else "application/json",
+                }
 
-            payload = {
-                "messages": [
-                {
-      "role": "user",
-      "content": [
-        {
-          "type": "text",
-          "text": "What is in this image?"
-        },
-        {
-          "type": "image_url",
-          "image_url": {
-            "url": "https://assets.ngc.nvidia.com/products/api-catalog/phi-3-5-vision/example1b.jpg"
-          }
-        }
-      ]
-    }
-  ],
-  "model": "google/gemma-4-31b-it",
-  "chat_template_kwargs": {
-    "enable_thinking": True
-  },
-  "max_tokens": 16384,
-  "stream": stream,
-  "temperature": 1,
-  "top_p": 0.95
-}
+                payload = {
+                  "messages": [
+                    {
+                      "role": "user",
+                      "content": [
+                        {
+                          "type": "text",
+                          "text": "What is in this image?"
+                          },
+                          {
+                            "type": "image_url",
+                            "image_url": {
+                              "url": "https://assets.ngc.nvidia.com/products/api-catalog/phi-3-5-vision/example1b.jpg"
+                            }
+                          }
+                        ]
+                      }
+                    ],
+                    "model": "google/gemma-4-31b-it",
+                    "chat_template_kwargs": {
+                      "enable_thinking": True
+                    },
+                    "max_tokens": 16384,
+                    "stream": stream,
+                    "temperature": 1,
+                    "top_p": 0.95
+                 }
 
 response = requests.post(invoke_url, headers=headers, json=payload, stream=stream)
 if stream:
